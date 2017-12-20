72205f476f307edafc4eea8dc0d531439ef19064
cjm <163mail-cjm@163.com>
2017-12-20 17:53:48 +0800

    对象的封装

--- 5-object/5-object.js
+++ 5-object/5-object.js
@@ -1,14 +1,53 @@
 var God = {
     version: '1.0',
+
     extends(name, obj) {
+        this.init();
         if ('data' in obj && 'methods' in obj &&
             typeof obj.data == 'function' && typeof obj.methods == 'object') {
             var getData = obj.data();
             if (typeof getData == 'object') {
-                Object.assign(obj.methods, getData);
+                Object.assign(obj.methods, getData, this);
                 this[name] = obj;   // 给God对象设置一个属性
             }
-        }
+        };
+    },
+
+    init() {
+        // 沈逸的方法
+        Object.defineProperties(this, {
+            'init': {
+                enumerable: false
+            },
+            'extends': {
+                enumerable: false
+            }
+        });
+
+        var keys = Object.keys(this);
+        this.$global = {};
+        
+        /*// function中的this代表这个函数，而箭头函数中的this却能代表所属的对象
+        keys.forEach(function (key) {
+            if (typeof key != 'function') {
+                this.$global[key] = this[key];  // this代表这个函数
+            }
+        })
+        // 我的方法
+        keys.forEach((key) => {
+            if (typeof this[key] != 'function') {
+                this.$global[key] = this[key];
+            }
+        })*/
+        // 沈逸的方法：在获取keys前，设置this的不可枚举属性
+        keys.forEach((key) => {
+            this.$global[key] = this[key];
+        });
+        // 为什么这样不能冻结$global属性，而要用freeze方法
+        /*Object.defineProperty(this, '$global', {
+            writable: false
+        });*/
+        Object.freeze(this.$global);
     }
 };
 
@@ -22,8 +61,9 @@ var news = {
 
     methods: {
         show() {
-            //alert(this.$global.version);
-            alert(this.title)
+            this.$global.version = '2.0';
+            alert(this.$global.version);
+            //alert(this.title)
         }
     }
 };
---
+++ 5-object/55-object.js
@@ -0,0 +1,11 @@
+var family = {
+    father: 'cjm',
+    mother: 'ly',
+    sons: ['cab', 'cabi'],
+
+    show() {
+       console.log('this methods')
+    }
+};
+
+console.log(Object.keys(family));
