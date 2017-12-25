b5640f8b8f28804db76eff324e7ac7718036b0b4
cjm <163mail-cjm@163.com>
2017-12-25 17:54:34 +0800

    小幅改动

--- 5-object/5-object.js
+++ 5-object/5-object.js
@@ -23,10 +23,8 @@ var God = {
                 enumerable: false
             }
         });
-
         var keys = Object.keys(this);
         this.$global = {};
-        
         /*// function中的this代表这个函数，而箭头函数中的this却能代表所属的对象
         keys.forEach(function (key) {
             if (typeof key != 'function') {
@@ -43,11 +41,31 @@ var God = {
         keys.forEach((key) => {
             this.$global[key] = this[key];
         });
-        // 为什么这样不能冻结$global属性，而要用freeze方法
-        /*Object.defineProperty(this, '$global', {
+        /*// 为什么这样不能冻结$global属性，而要用freeze方法
+        Object.defineProperty(this, '$global', {
             writable: false
         });*/
-        Object.freeze(this.$global);
+        //Object.freeze(this.$global);
+        this.watch('version', this.$global);
+    },
+
+    watch(keyName, obj) {
+        // 为什么要使用_key这个局部变量（或叫私有变量）呢？
+        // 错误：too much recursion，先不研究这个！
+        var _key = '_' + keyName;
+        if(!(_key in obj)) {
+            obj[_key] = obj[keyName]
+        }
+
+        Object.defineProperty(obj, keyName, {
+            set(value) {
+                alert(keyName + ' 属性被修改了');
+                obj[_key] = value;
+            },
+            get() {
+                return obj[_key];
+            }
+        })
     }
 };
 
@@ -61,15 +79,14 @@ var news = {
 
     methods: {
         show() {
-            this.$global.version = '2.0';
             alert(this.$global.version);
-            //alert(this.title)
+            this.$global.version = '3.0';
+            alert(this.$global.version);
         }
     }
 };
 
 /*Object.assign(news.methods, news.data());
 news.methods.show();*/
-
 God.extends('news', news);
 God.news.methods.show();
